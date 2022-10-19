57356e1eb88cc564d445f430aaa83dce4decd5e2
Snow <skymygo6809@gmail.com>
2022-10-19 16:32:26 +0900

    feat: "Django secret"

--- Django_app/apps/models.py
+++ Django_app/apps/models.py
@@ -2,5 +2,12 @@ from django.db import models
 # Create your models here.
 from time import sleep
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class User(AbstractUser):
+    src = models.CharField("가입방법", max_length=20)
+    share_link = models.CharField("공유 링크", max_length=100)
+    created_on = models.DateTimeField("등록일자", auto_now_add=True)
+    updated_on = models.DateTimeField("수정일자", auto_now=True)
+
---
+++ Django_app/secret/config.py
@@ -0,0 +1,3 @@
+#INPUT YOUR SECRET
+
+SECRET = 'django-insecure-3$hc!=^#8rz_wp2@zv7z$t$$^ia(9_md3ku*0@j4sqk31bj@vr' #TEST
