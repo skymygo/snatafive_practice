796416235b639229137df923794d1ae34056e32c
Snow <skymygo6809@gmail.com>
2022-11-01 23:00:24 +0900

    "feat": "models 구조 변경"
    사용자의 보드를 하나로 한정짓지 않고, 추후 보드 개수를 늘리는 등의 확장성을 위해 보드 table 추가.
    
    Django에 대해 좀 더 공부하고 구현하려고 현재 https://developer.mozilla.org/ko/docs/Learn/Server-side/Django 공부중이므로 구현에 시간이 걸릴 예정.

--- Django_app/apps/models.py
+++ Django_app/apps/models.py
@@ -15,13 +15,35 @@ class User(AbstractUser):
     class Meta:
         db_table = 'user'
 
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+class RollingPaperBoard(models.Model):
+    name = models.CharField('보드이름', max_length=100)
+    board_image = models.IntegerField('보드 그림 종류')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField('등록일자', auto_now_add=True)
+    updated_on = models.DateTimeField('수정일자', auto_now=True)
+    opened_at = models.DateTimeField('공개일자')
+
+    class Meta:
+        db_table = 'rolling_paper_board'
+
+    def __str__(self):
+        return self.name
+
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_message_set')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver_message_set')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    rolling_paper_board = models.ForeignKey(RollingPaperBoard, on_delete=models.CASCADE)
     contents = models.CharField(verbose_name='메시지 내용', max_length=500)
     acc_type = models.IntegerField(verbose_name='악세사리 타입')
+    created_on = models.DateTimeField('등록일자', auto_now_add=True)
+    updated_on = models.DateTimeField('수정일자', auto_now=True)
 
     class Meta:
         db_table = 'tree_message'
         verbose_name = '메시지'
         verbose_name_plural = '메시지 목록'
+
+    def __str__(self):
+        return f"[{self.created_on}]: {self.contents}"
